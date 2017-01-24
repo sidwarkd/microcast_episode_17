d8bba36dfd5544a034ae7cf632c2a07142353465
Sameer Shaik <sameer.shaik@studentpartner.com>
2017-01-24 19:58:59 +0530

    updated app.js

--- client/app.js
+++ client/app.js
@@ -3,6 +3,8 @@ var socket = require("socket.io-client")(config.server_url);
 var gpio = require("rpi-gpio");
 var sensorLib = require('node-dht-sensor');
 
+var data={};
+
 process.on("SIGINT", function(){
   gpio.write(config.led, 1, function(){
     gpio.destroy(function(){
@@ -23,10 +25,7 @@ var dht_sensor = {
         var readout = sensorLib.read();
         console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
             'humidity: ' + readout.humidity.toFixed(2) + '%');
-      socket.emit('temp',{
-      temp:readout.temperature.toFixed(2)
-      
-      });
+      data[temp]=readout.temperature;
         setTimeout(function () {
             dht_sensor.read();
         }, 2000);
@@ -41,6 +40,7 @@ if (dht_sensor.initialize()) {
 
 socket.on("connect", function(){
   console.log("Connected to server");
+  socket.emit('msg',data);
   socket.on("updateState", function(state){
     console.log("The new state is: " + state);
     gpio.write(config.led, !state);
